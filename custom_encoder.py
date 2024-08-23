5fb1f7d92e7fcd6dfd622ba70847a487e47b6b0a
Anees Ahmad <93769807+Anees004@users.noreply.github.com>
2024-08-23 16:56:28 +0500

    Add files via upload

---
+++ custom_encoder.py
@@ -0,0 +1,12 @@
+from sklearn.preprocessing import LabelEncoder
+import numpy as np
+
+class ExtendedLabelEncoder(LabelEncoder):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+    def transform(self, y):
+        unseen_labels = set(y) - set(self.classes_)
+        if unseen_labels:
+            self.classes_ = np.append(self.classes_, list(unseen_labels))
+        return super().transform(y)
