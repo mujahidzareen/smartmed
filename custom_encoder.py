f7abe11208f0341667df3dd43a2e686cebedec98
Anees Ahmad <93769807+Anees004@users.noreply.github.com>
2024-08-27 17:15:06 +0500

    Update custom_encoder.py

--- custom_encoder.py
+++ custom_encoder.py
@@ -2,7 +2,6 @@ from sklearn.preprocessing import LabelEncoder
 import numpy as np
 
 class ExtendedLabelEncoder(LabelEncoder):
-    print("I am inside the encoder function")
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
 
